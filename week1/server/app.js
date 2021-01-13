17b8b908108f42c066daaf097e2e50d2f6feadfe
eric22r <52829348+eric22r@users.noreply.github.com>
2021-01-13 12:54:41 -0500

    second commit

--- week1/server/app.js
+++ week1/server/app.js
@@ -1,7 +1,7 @@
 //app is the entry point of application
 function sendMsg(){
 
-    console.log("Wouldn't you you like to be a pepper too???");
+    console.log("Hello World");
 }
 
-//sendMsg()
+sendMsg()
